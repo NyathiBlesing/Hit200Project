aaf08732e740696fcae83000b805e742552830df
Innocen304 <innocentmundanga28@gmail.com>
2025-05-01 23:47:13 +0200

    Add hit200project.onrender.com to ALLOWED_HOSTS by default

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'insecure-dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,hit200project.onrender.com').split(',')
 
 
 # Application definition
